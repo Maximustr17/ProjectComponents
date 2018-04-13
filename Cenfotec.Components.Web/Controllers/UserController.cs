5ce071d66452a5bcee24c92bec6ce31a968069d7
lindsalazar <lsalazarc@ucenfotec.ac.cr>
2018-04-13 07:58:48 -0600

    Web

---
+++ Cenfotec.Components.Web/Controllers/UserController.cs
@@ -0,0 +1,46 @@
+﻿using Cenfotec.Components.AS.Mantenimientos;
+using Cenfotec.Components.Entities.Mantenimientos.Entrada;
+using Cenfotec.Components.Entities.Mantenimientos.Salida;
+using Cenfotec.Components.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cenfotec.Components.Web.Controllers
+{
+    public class UserController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SaveUser(UserModels modelo)
+        {
+            string respuesta = "99";
+
+            MantenimientosAS mantenimientosAS = null;
+            SaveUserReq oReq = new SaveUserReq();
+            SaveUserRes oRes = null;
+
+            try
+            {               
+                if (modelo != null)
+                {
+                    
+                }
+            }
+
+            catch (Exception ex)
+            {
+
+            }
+
+            return Json(new { respuesta = "00" });
+        }
+    }
+}
---
+++ Cenfotec.Components.Web/Models/UserModels.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cenfotec.Components.Web.Models
+{
+    [Serializable]
+    public class UserModels
+    {
+        #region Propiedades
+        public Guid user_id { get; set; }
+        public string display_name { get; set; }
+        public string email { get; set; }
+        public string spotify_url { get; set; }
+        public string href { get; set; }
+        public string id { get; set; }
+        #endregion
+
+        #region Métodos
+
+        #endregion
+    }
+}
